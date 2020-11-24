b7687845e705a93e6e54b3bdd8951ae287b06488
ericting2000 <erictingting2000@gmail.com>
2020-11-25 00:50:35 +0800

    rearrange matrix.h

--- matrix.h
+++ matrix.h
@@ -1,19 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
-typedef struct{
+typedef struct {
     int row, col, value;
     char name[100];
 } Matrix;
 
-Matrix m[100][101]={};
-Matrix t[100][101]={};
-Matrix a[100][101]={};
+Matrix m[100][101] = {};
+Matrix t[100][101] = {};
+Matrix a[100][101] = {};
 
 int insert_matrix(int index);
 void read_matrix(int numbs);
-void print_matrix(char name[100], char type,  int numbs);
+void print_matrix(char name[100], char type, int numbs);
 void submatrix(int numbs);
 void transposition(int numbs);
 void addition(int numbs);
